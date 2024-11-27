21270937a293aeb0f9bd646b0df4d8e4fadeb129
MANCHI LAKSHMI NARASIMHULU <m.narasimhans5974@gmail.com>
2024-11-27 07:41:14 +0530

    Blood donation eligibility checker.c
    To Check eligibility for blood donation using C program

---
+++ Blood donation eligibility checker.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int age;
+printf("Enter your age: ");
+scanf("%d",&age);
+if(age>=18){
+printf("Your are eligible to Donate your BLOOD.");
+}
+else{
+printf("Your are not eligible to Donate your BLOOD.");
+}
+}
