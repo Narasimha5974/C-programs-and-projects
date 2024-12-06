dbe1e6ecf9edfd24a33a21871f7f3f81e2460f50
MANCHI LAKSHMI NARASIMHULU <m.narasimhans5974@gmail.com>
2024-12-06 15:50:41 +0530

    SIZE OF STRING ARRAY USING "sizeof" OPERATOR.c

---
+++ SIZE OF STRING ARRAY USING "sizeof" OPERATOR.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+ char str[]={'h','e','l','l','o'};
+ printf("STRING ARRAY: %s\n",str);
+ printf("SIZE OF STRING ARRAY: %d\n",sizeof(str));
+ 
+ 
+ 
+ int arr[]={5,6,7,8,9,12};
+ printf("\nSIZE OF INTEGER ARRAY: %d",sizeof(arr));
+}
