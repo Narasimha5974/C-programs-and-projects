f1d6075cb4002a5670ebf626a3c5f5c61d22a423
MANCHI LAKSHMI NARASIMHULU <m.narasimhans5974@gmail.com>
2025-01-20 21:00:12 +0530

    Playing With Characters.c
    Hacker Rank

--- Playing With Characters.c
+++ Playing With Characters.c
@@ -1,11 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
 
-int main() 
-{
-char ch;
-scanf("%c", &ch);
-printf("%c\n", ch);
-printf("Language\n");
-printf("Welcome To C!!");
+int main(){
+
+char ch, s[20] , sen[30];
+scanf("%c",&ch);
+scanf("%s",s);
+scanf("\n");
+scanf("%[^\n]%*c",sen);
+printf("%c\n",ch);
+printf("%s\n",s);
+printf("%s",sen);
+
+return 0;
+
 }
- 
